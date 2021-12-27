d995f8d2476a449b1cfd3e753d2392a03f622338
abhinavsinghyadav5 <91362425+abhinavsinghyadav5@users.noreply.github.com>
2021-12-27 18:41:27 +0530

    Add files via upload

---
+++ operator+.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class marks
+{
+    int intmarks;
+    int extmarks;
+    public:
+     marks()
+     {
+         intmarks=0;
+         extmarks=0;
+     }
+     marks(int im,int em)
+     {
+         intmarks=im;
+         extmarks=em;
+     }
+     void display()
+     {
+         cout<<intmarks<<endl<<extmarks<<endl;
+     }
+     marks operator-(marks m)
+     {
+         marks temp;
+         temp.intmarks=intmarks-m.intmarks;
+         temp.extmarks=extmarks-m.extmarks;
+         return temp;
+     }
+};
+
+int main()
+{
+    marks m1(200,100),m2(20,78);
+    marks m3=m1-m2;
+    m3.display();
+    return 0;
+}
